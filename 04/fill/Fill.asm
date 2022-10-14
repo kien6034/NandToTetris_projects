8c48be3ee7a1db30c86f57e42e3dc90404507506
kien6034 <trinhtrungkientb99@gmail.com>
2022-10-15 00:24:00 +0700

    feat: 04-fill: dectect the keyboard and simple setting the value

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,12 +3,42 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input.
+// Runs an infinite loop that listens to the keyboard input. 
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+// i.e. writes "black" in every pixel. When no key is pressed, the
+// program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+
+(CHECK_KEY_PRESSED)
+@KBD 
+D=M 
+@BLACKEN 
+D; JGT 
+@WHITEN
+0; JMP 
+
+(BLACKEN)
+//blackend the screen 
+@R1
+D=A 
+@R0
+M=D 
+
+//go back to the loop 
+@CHECK_KEY_PRESSED
+0;JMP
+
+
+
+(WHITEN)
+//whiten the screen
+@R0 
+D=A
+@R0 
+M=D
+
+// go back to the loop
+@CHECK_KEY_PRESSED
+0;JMP
